7cadb623506cafffd1d6df856e49984b6d4d5c89
Igor Kamyshev <igor@kamyshev.me>
2019-02-23 21:25:07 +0200

    fix(post-install): ensure husky installed

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,8 +1,15 @@
 const path = require('path')
 const spawn = require('cross-spawn')
+const fs = require('fs')
 
 module.exports = async ({ projectPath }) => {
-  const huskyInstaller = path.join(projectPath, './node_modules/husky/husky.js')
+  const huskyDir = path.join(projectPath, './node_modules/husky')
+
+  if (!fs.existsSync(huskyDir)) {
+    return { status: 0 }
+  }
+
+  const huskyInstaller = path.join(huskyDir, 'husky.js')
 
   return spawn.sync('node', [huskyInstaller, 'install'], { stdio: 'inherit' })
 }
