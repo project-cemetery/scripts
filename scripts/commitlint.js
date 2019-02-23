baf73242c87b9cc2f9d41b6f1e2d700b13b1bbd7
Igor Kamyshev <igor@kamyshev.me>
2019-02-23 22:06:06 +0200

    fix(commitlint): fix typo

--- scripts/commitlint.js
+++ scripts/commitlint.js
@@ -1,5 +1,9 @@
 const spawn = require('cross-spawn')
 
 module.exports = async () => {
-  return spawn.sync('commitlint', ['--config', rcPath], { stdio: 'inherit' })
+  return spawn.sync(
+    'commitlint',
+    ['-E', 'HUSKY_GIT_PARAMS', '-x', '@commitlint/config-conventional'],
+    { stdio: 'inherit' },
+  )
 }
