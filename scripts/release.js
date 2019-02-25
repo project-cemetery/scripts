498a7add574795416074f8aa3412e9bafae397e3
Igor Kamyshev <igor@kamyshev.me>
2019-02-25 19:28:22 +0200

    fix(release): fix args for release command

--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,7 @@
 const spawn = require('cross-spawn')
 
 module.exports = async ({ projectPath }) => {
-  const result = spawn.sync('standard-version', ['--path', projectPath], {
+  const result = spawn.sync('standard-version', ['--', '--path', projectPath], {
     stdio: 'inherit',
   })
 
