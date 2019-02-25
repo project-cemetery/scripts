7132f70c7f2d60b3e2f03f582e4e9c1525f0d657
Igor Kamyshev <igor@kamyshev.me>
2019-02-25 19:36:01 +0200

    fix(release): remove unused options in release

--- scripts/release.js
+++ scripts/release.js
@@ -1,7 +1,7 @@
 const spawn = require('cross-spawn')
 
-module.exports = async ({ projectPath }) => {
-  const result = spawn.sync('standard-version', ['--', '--path', projectPath], {
+module.exports = async () => {
+  const result = spawn.sync('standard-version', {
     stdio: 'inherit',
   })
 
