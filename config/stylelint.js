e9734944d729df30f3c40d8fc96c78b23bac9a1c
iwanttobealight <iwanttobealight@yandex.ru>
2019-05-20 15:23:59 +0300

    fix(stylelint): delete extra string

--- config/stylelint.js
+++ config/stylelint.js
@@ -1,5 +1,4 @@
 module.exports = {
-  ignoreAtRules: ['at-rule-no-unknown', 'mixin'],
   extends: ['stylelint-config-recommended', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
   rules: {
