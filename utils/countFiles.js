94c64d99c60003164f4c3336a6622ae76e86b1c2
Igor Kamyshev <igor@kamyshev.me>
2019-06-28 13:58:17 +0300

    perf: actually replace slow find by custom fast versiob

--- utils/countFiles.js
+++ utils/countFiles.js
@@ -1,22 +1,18 @@
-const find = require('find')
-
 const ignoreToStirng = require('../utils/ignoreToString')
+const { findFiles } = require('./findFiles')
 
 // We must skip typings
 const filterTypings = file => !file.endsWith('d.ts')
 
 const countFiles = (exts, projectPath, ignoreFile) => {
   const extString = exts.join('|')
+  const ignorePaths = ignoreToStirng(projectPath, ignoreFile)
 
-  return new Promise(resolve => {
-    const ignorePaths = ignoreToStirng(projectPath, ignoreFile)
-
-    find.file(
-      new RegExp(`^((?!${ignorePaths}).)*(${extString})$`),
-      projectPath,
-      files => resolve(files.filter(filterTypings).length),
-    )
-  })
+  return findFiles(
+    projectPath,
+    ['node_modules', '.git'],
+    new RegExp(`^((?!${ignorePaths}).)*(${extString})$`),
+  ).then(files => files.filter(filterTypings).length)
 }
 
 module.exports = countFiles
--- utils/getAllPackages.js
+++ utils/getAllPackages.js
@@ -1,42 +1,9 @@
 const fse = require('fs-extra')
-const { join } = require('path')
-const { flatten } = require('lodash')
 
-const customFind = async (projectPath, ignoreNames, targetName) => {
-  const foundFiles = []
-  const files = await fse.readdir(projectPath)
-
-  await Promise.all(
-    files.map(async rawName => {
-      const name = join(projectPath, rawName)
-
-      if (ignoreNames.some(ignoreName => name.includes(ignoreName))) {
-        return null
-      }
-
-      if (name.includes(targetName)) {
-        foundFiles.push(name)
-        return null
-      }
-
-      const isDirectory = await fse.lstatSync(name).isDirectory()
-
-      if (isDirectory) {
-        const nestedFound = await customFind(name, ignoreNames, targetName)
-        if (nestedFound.length > 0) {
-          foundFiles.push(nestedFound)
-        }
-      }
-
-      return null
-    }),
-  )
-
-  return flatten(foundFiles)
-}
+const { findFiles } = require('./findFiles')
 
 const getAllPackages = async projectPath => {
-  const files = await customFind(
+  const files = await findFiles(
     projectPath,
     ['node_modules', '.git'],
     'package.json',
