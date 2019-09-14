0b6417180ccfd434fe652c629f2fad1248fd948e
Igor Kamyshev <igor@kamyshev.me>
2019-09-14 09:24:49 +0300

    fix(lint): disable incorrect rule for sorting imports

--- config/eslint-base.js
+++ config/eslint-base.js
@@ -218,7 +218,7 @@ module.exports = {
           '/^@front/',
           ['parent', 'sibling', 'index'],
         ],
-        alphabetize: { order: 'asc', ignoreCase: true },
+        alphabetize: { order: 'ignore', ignoreCase: true },
       },
     ],
   },
