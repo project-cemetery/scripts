fdcb6186b5f3678aa86e6b37507f6fbf0b66dfe9
Igor Kamyshev <igor@kamyshev.me>
2020-04-12 07:41:35 +0700

    feat(migrate): add badge

--- migrate/index.js
+++ migrate/index.js
@@ -1,4 +1,5 @@
-const { uninstall, packageJson } = require('mrm-core');
+const { uninstall, packageJson, markdown } = require('mrm-core');
+const { version } = require('../package.json')
 
 function task() {
   uninstall('@solid-soda/scripts')
@@ -6,6 +7,13 @@ function task() {
   packageJson()
     .removeScript('s')
     .save()
+
+    markdown('README.md')
+    .addBadge(
+        `https://img.shields.io/static/v1?label=@solid-soda/scripts&message=${version}`,
+        'https://github.com/solid-soda/scripts',
+        `Scripts sets up by @solid-soda/scripts v${version}`
+    )
 }
 
 task.description = 'Migrate from @solid-soda/scripts v1.x.x';
