9a723dcc643e927162bd6f448ee67a2a4fda8498
Igor Kamyshev <igor@kamyshev.me>
2020-04-12 11:13:17 +0700

    fix(migrate): fix badge adder

--- migrate/index.js
+++ migrate/index.js
@@ -8,13 +8,17 @@ function task() {
     .removeScript('s')
     .save()
 
-    markdown('README.md')
-    .addBadge(
+  const readme = markdown('README.md')
+  if (readme.exists()) {
+    readme
+      .addBadge(
         `https://img.shields.io/static/v1?label=@solid-soda/scripts&message=${version}&color=75ddf4`,
         'https://github.com/solid-soda/scripts',
         `Scripts sets up by @solid-soda/scripts v${version}`
-    )
-    .save()
+      )
+      .save()
+  }
+
 }
 
 task.description = 'Migrate from @solid-soda/scripts v1.x.x';
